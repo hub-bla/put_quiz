dfdce06f16a49937145ea562402f1b6fdcca9d5f
Hubert Błaszczyk <56601011+hub-bla@users.noreply.github.com>
2024-11-13 10:26:06 +0100

    update file structure and add react router

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import * as path from "path"
+import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	base: "/",
+	plugins: [react()],
+	resolve: {
+		alias: [
+			{
+				find: "@components",
+				replacement: path.resolve(__dirname, "./src/components/"),
+			},
+			{ find: "@utils", replacement: path.resolve(__dirname, "./src/utils/") },
+			{
+				find: "@assets",
+				replacement: path.resolve(__dirname, "./src/assets/"),
+			},
+			{ find: "@pages", replacement: path.resolve(__dirname, "./src/pages/") },
+			{ find: "@", replacement: path.resolve(__dirname, "./src/") },
+		],
+	},
 })
