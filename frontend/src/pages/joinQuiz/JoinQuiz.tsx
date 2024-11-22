3e25124184bc8cd462d8d0b30b0b410d797cc557
Hubert Błaszczyk <56601011+hub-bla@users.noreply.github.com>
2024-11-22 12:08:14 +0100

    implement joinQuiz page

--- frontend/src/pages/joinQuiz/JoinQuiz.tsx
+++ frontend/src/pages/joinQuiz/JoinQuiz.tsx
@@ -1,8 +1,41 @@
+import { useState } from "react"
+import "./joinQuiz.css"
+
 export const JoinQuiz: React.FC = () => {
+	const [formData, setFromData] = useState({
+		gameCode: null,
+		username: null,
+	})
+
+	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const { id, value } = e.target
+
+		setFromData((prevFormData) => ({ ...prevFormData, [id]: value }))
+	}
+
+	const handleSubmit = () => {
+		console.log("submitted")
+	}
+
 	return (
-		<>
-			<input type='text' />
-			<input type='text' />
-		</>
+		<form className='join-form' onSubmit={handleSubmit}>
+			<input
+				className='form-input'
+				id='gameCode'
+				type='text'
+				placeholder='Enter game code'
+				onChange={handleInputChange}
+				required
+			/>
+			<input
+				className='form-input'
+				id='username'
+				type='text'
+				placeholder='Enter your username'
+				onChange={handleInputChange}
+				required
+			/>
+			<button type='submit'>Play</button>
+		</form>
 	)
 }
